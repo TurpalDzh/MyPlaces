00fb81d1876c097ac98ca956c91a1b3790c25b9e
TurpalDzh <140629979+TurpalDzh@users.noreply.github.com>
2023-09-12 13:30:03 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Turpal Dzhantamirov on 12.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
